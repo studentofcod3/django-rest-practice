ee49f566a83c6591187ee8b64ffa6764195c8632
Ayyoub Maknassa <ayyoub.maknassa@hotmail.com>
2024-08-28 11:13:50 +0100

    Deep dive filtering, ordering, pagination and search fields
    - Filtering using django-filter
    - Ordering using built in DRF feature, ordering fields with defaults
    - Pagination using built in DRF feature, allow client to paginate with defaults
    - Search fields using built in DRF feature

--- taskmanager/tasks/api_views.py
+++ taskmanager/tasks/api_views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets, mixins
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, viewsets, mixins
+from rest_framework.pagination import PageNumberPagination
 from tasks.models import Task, Category
 from tasks.serializers import TaskListSerializer, TaskSerializer, CategorySerializer, TaskUpdateSerializer
 from rest_framework.decorators import action
@@ -6,6 +8,10 @@ from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
+class TaskPagination():
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
 class TaskViewSet(
     viewsets.GenericViewSet,
@@ -16,6 +22,16 @@ class TaskViewSet(
 ):
     permission_classes = [IsAuthenticated]
     authentication_classes = [TokenAuthentication]
+    pagination_class = TaskPagination
+    filter_backends = [
+        DjangoFilterBackend, 
+        filters.OrderingFilter,
+        filters.SearchFilter,
+    ]
+    filterset_fields = ['completed', 'category']
+    ordering_fields = ['created_at']
+    ordering = ['-created_at'] # Default ordering
+    search_fields = ['title', 'description', 'category__name']
 
     def get_queryset(self):
         return Task.objects.filter(completed=False)
